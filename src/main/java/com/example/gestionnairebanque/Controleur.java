02274b1bb74e3c97f30ebac31a100f598987ce7b
biadzok <biadzok4@gmail.com>
2023-03-31 13:56:34 +0200

    Premier commit, ajout du controleur principal

---
+++ src/main/java/com/example/gestionnairebanque/Controleur.java
@@ -0,0 +1,27 @@
+package com.example.gestionnairebanque;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Controleur extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Controleur.class.getResource("IHM.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 200, Color.LIGHTGRAY);
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("src/main/resources/3il_logo.jpg")));
+        stage.setTitle("Gestion banquaire");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
