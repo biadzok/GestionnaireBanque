5fe0f7424f8a7dd9f0e162f12ca1bc340df3eea2
biadzok <biadzok4@gmail.com>
2023-03-31 14:26:11 +0200

    Ajout de la classe transaction, de ses propriétés et leurs getters

---
+++ src/main/java/com/example/model/Transaction.java
@@ -0,0 +1,43 @@
+package com.example.model;
+
+import java.io.*;
+
+public class Transaction implements Serializable {
+    private String Nom;
+    private char type;
+    private double Montant;
+
+    public char getType() {
+        return type;
+    }
+
+    public double getMontant() {
+        return Montant;
+    }
+
+    public String getNom() {
+        return Nom;
+    }
+
+    public void setMontant(double montant) {
+        Montant = montant;
+    }
+
+    public void setNom(String nom) {
+        Nom = nom;
+    }
+
+    public void setType(char type) {
+        this.type = type;
+    }
+
+    public Transaction(String nom, char type, double montant) {
+
+    }
+
+    public Transaction(String path) {
+    }
+
+    public void saveTransaction(String path) {
+    }
+}
